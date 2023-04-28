85483fb5e8cc0568132b4462a65a96492ebe4cd9
freda <ndyfreda@gmail.com>
2023-04-28 17:23:06 -0400

    tasklister mini project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form');
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    listTask(e.target.newTaskDescription.value);
+    form.reset();
+  })
 });
+
+function listTask(addTask){
+  let li = document.createElement('li');
+  li.textContent = `${addTask} `;
+  let btn = document.createElement('button');
+  btn.textContent = 'x';
+  btn.addEventListener('click', deleteTask);
+  li.appendChild(btn).style.color='red';
+  document.querySelector('#tasks').appendChild(li);
+}
+
+function deleteTask(e){
+  e.target.parentNode.remove();
+}
